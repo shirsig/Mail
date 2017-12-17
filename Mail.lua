c6919faabcc67ae4fbceccbd7bff87c2a8f0e9b1
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2017-12-17 12:57:27 +0100

    safety update for opening

--- Mail.lua
+++ Mail.lua
@@ -202,6 +202,44 @@ function Inbox_Load()
 end
 
 do
+	local i, update
+	local f = CreateFrame'Frame'
+	f:Hide()
+	f:SetScript('OnUpdate', function()
+		if update then
+			update = false
+			local _, _, _, _, _, COD = GetInboxHeaderInfo(i)
+			if i > GetInboxNumItems() then
+				Inbox_Abort()
+			elseif Inbox_Skip or COD > 0 then
+				Inbox_Skip = false
+				i = i + 1
+			else
+				Inbox_Open(i)
+			end
+		end
+	end)
+	function MAIL_INBOX_UPDATE()
+		if Inbox_opening then
+			update = true
+		end
+	end
+	function Inbox_OpenAll()
+		Inbox_opening = true
+		Inbox_UpdateLock()
+		i = 1
+		Inbox_skip = false
+		update = true
+		f:Show()
+	end
+	function Inbox_Abort()
+		Inbox_opening = false
+		Inbox_UpdateLock()
+		f:Hide()
+	end
+end
+
+do
 	local i
 	local function update()
 		local _, _, _, _, _, COD = GetInboxHeaderInfo(i)
